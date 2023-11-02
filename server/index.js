e9e7ae0e05da88f859c4479412185d80e1ebc3f9
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-11-03 01:15:14 +0530

    socket set basic frontend setup ready

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
+    });
+  });
+  socket.on(
+    "answerCall",
+    (data) => io.to(data.to).emit("callAccepted"),
+    data.signal
+  );
+});
+
+server.listen(3000, () => console.log("server started"));
